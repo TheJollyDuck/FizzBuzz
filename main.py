48cf7f36c50a431693a87d75fe9d25e58bd8b815
TheJollyDuck <shauno1216@gmail.com>
2020-06-01 00:40:10 +0100

    This is an initial test of Git

---
+++ main.py
@@ -0,0 +1,17 @@
+# Shaun Recto
+# FizzBuzz
+# This program prints out numbers but for
+# a specifc multiple of a number with a word
+
+numList = [3,5,10,13]
+wordlist = ["fizz","buzz","splonk","kaboom"]
+
+for i in range(0,1000):
+    output = ""
+    for j in range(0,len(numList)):
+        if i%numList[j] == 0:
+            output += wordlist[j]
+    if output == "":
+        print(i)
+    else:
+        print(output)
