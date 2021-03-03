06c02a661883a8c2559d56a984682ecff0ceb015
TheJollyDuck <shauno1216@gmail.com>
2021-03-03 02:03:50 +0000

    Adds base file for MALTAB version

---
+++ fizzbuzz.m
@@ -0,0 +1,24 @@
+%% TheJollyDuck
+%%
+%% FizzBuzz
+%% MATLAB/Octave Implementation
+%%
+%%
+
+pairs = [
+  3, "Fizz";
+  5, "Buzz";
+  6, "Breeze"
+];
+
+numLimit = input("How many numbers do you want to go up to [int]: ");
+t = 1:numLimit;
+
+for T = t
+    for n = T
+      if ()
+        
+      end
+    end
+
+end
